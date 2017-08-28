2cda28404cc58259b232d5f48f732f2608f95a10
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-08-28 17:37:27 -0400

    Add some setup code

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,54 @@
 class Elevator {
-  constructor(){
+  constructor() {
+    // floor where the elevator currently is
     this.floor      = 0;
+
+    // current direction that the elevator is moving in
+    // ("up" or "down")
+    this.direction = 'up';
+
+    // top floor of the building (max value of "this.floor")
     this.MAXFLOOR   = 10;
-    this.requests   = [];
+
+    // an array of person objects
+    // represent people on the elevator waiting to be dropped off
+    this.passengers = [];
+
+    // an array of person objects
+    // represents people waiting to be picked up
+    this.waitingList = [];
+
+    // an array of numbers of the floors which the elevator must stop
+    // (either to drop off or pick up)
+    this.requests = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
+
+  start() {
+    // begin a setInterval that calls "update()" every second
+  }
+
+  stop() {
+    // cancel the setInterval started by "start()"
+  }
+
+  update() {
+    // (for now) will only call "log()"
+  }
+
+  log() {
+    // displays the current direction and current floor
+    // Examples:
+    // "Direction: down | Floor 3"
+    // "Direction: up   | Floor 5"
+  }
+
+
   _passengersEnter() { }
   _passengersLeave() { }
   floorUp() { }
   floorDown() { }
   call() { }
-  log() { }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,32 @@
+// import the "elevator.js" file into the program
 const Elevator = require('./elevator.js');
+
+// import the "person.js" file into the program
+const Person = require('./person.js');
+
+
+// create our elevator
+const myElevator = new Elevator();
+
+
+// start the elevator's automatic movement
+myElevator.start();
+
+
+// stop the elevator after 20 seconds
+setTimeout(() => {
+    myElevator.stop();
+}, 20000);
+
+
+
+// For iteration 2: call "floorUp()" and "floorDown()"
+
+
+// myElevator.floorUp();
+// myElevator.floorUp();
+// myElevator.floorUp();
+// myElevator.floorUp();
+// 
+//
+// myElevator.floorDown();
